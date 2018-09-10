4bb9221ea1593e60e266d965328d9a9c1fa09799
leon846666 <leon846666@gmail.com>
2018-09-09 23:00:47 -0400

    read data from properties file and hot deployment

--- src/main/java/com/leon/firstController.java
+++ src/main/java/com/leon/firstController.java
@@ -1,14 +1,23 @@
 package com.leon;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class firstController {
 
+	@Autowired
+	private Environment env;
 	
 	@RequestMapping("/info")
 	public String info(){
-		return "hello world";
+		return "hello world"+env.getProperty("url");
+	}
+	
+	@RequestMapping("/info2")
+	public String info2(){
+		return "hello world by info2 "+env.getProperty("url");
 	}
 }
